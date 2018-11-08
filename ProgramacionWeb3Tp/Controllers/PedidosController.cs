a0ecd5da74363d05b688bc5222197bcce80bc6e3
Lucas Andres Miharu Ferreira <lmiharuferreira@alumno.unlam.edu.ar>
2018-11-08 16:06:17 -0300

    Update 8112018 SIN clases

---
+++ ProgramacionWeb3Tp/Controllers/PedidosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProgramacionWeb3Tp.Controllers
+{
+    public class PedidosController : Controller
+    {
+        // GET: Pedidos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult iniciar()
+        {
+            return View();
+        }
+
+    }
+}
