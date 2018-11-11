8d83ead19e2268a368bdc4143d4c1ca913b73796
Rama <radamisil@users.noreply.github.com>
2018-11-11 15:42:22 -0300

    ok

---
+++ ProgramacionWeb3Tp/Controllers/PedidosController.cs
@@ -0,0 +1,41 @@
+using ProgramacionWeb3Tp.Models;
+using ProgramacionWeb3Tp.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProgramacionWeb3Tp.Controllers
+{
+    public class PedidosController : Controller
+    {
+        private readonly PedidoServicio _pedidoServicio = new PedidoServicio();
+
+        [HttpGet]
+        public ActionResult iniciar()
+        {
+            Pedido pedido = new Pedido();
+
+            return View(pedido);
+        }
+       
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]  //Para prevenir ataques CSRF
+        public ActionResult RegistrarPedido(Pedido pedido)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                _pedidoServicio.SavePedido(pedido);
+                return View("iniciado");
+
+            }
+            return View("iniciado");
+
+        }
+
+    }
+}
